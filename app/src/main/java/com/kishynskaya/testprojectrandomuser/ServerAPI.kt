8d138945af11ebc8e900f491d7422d0f3bf5e748
mashazavolnyuk <mashazavolnyuk@yandex.ru>
2020-01-09 01:23:21 +0200

    cleaned project from tested logs

--- app/src/main/java/com/kishynskaya/testprojectrandomuser/ServerAPI.kt
+++ app/src/main/java/com/kishynskaya/testprojectrandomuser/ServerAPI.kt
@@ -1,8 +1,6 @@
 package com.kishynskaya.testprojectrandomuser
 
 import io.reactivex.Single
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -13,18 +11,10 @@ object ServerAPI {
 
     private const val BASE_URL = "https://randomuser.me/api/"
 
-    private fun setClient(): OkHttpClient {
-        val interceptor = HttpLoggingInterceptor()
-        interceptor.level = HttpLoggingInterceptor.Level.BODY
-        return OkHttpClient.Builder().addInterceptor(interceptor).build()
-    }
-
-
     private var builder: Retrofit.Builder =
         Retrofit.Builder()
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
-            .client(setClient())
             .baseUrl(BASE_URL)
 
     private val retrofit = builder.build()
