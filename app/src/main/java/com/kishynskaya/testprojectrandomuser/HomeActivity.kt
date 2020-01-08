251645b4f82fc64750787228348a26c939ced7a0
mashazavolnyuk <mashazavolnyuk@yandex.ru>
2020-01-09 01:59:31 +0200

    Cleaned

--- app/src/main/java/com/kishynskaya/testprojectrandomuser/HomeActivity.kt
+++ app/src/main/java/com/kishynskaya/testprojectrandomuser/HomeActivity.kt
@@ -2,7 +2,6 @@ package com.kishynskaya.testprojectrandomuser
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
